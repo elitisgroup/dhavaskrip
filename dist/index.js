ee4c1eef3107138d311ad7e4b1a3382ad9a0f04f
Chiphyr <alex@ascendcommunity.gq>
2019-12-27 19:14:03 +0000

    🚸 Allow 'compile' to be used over '--compile'

--- dist/index.js
+++ dist/index.js
@@ -8,12 +8,13 @@
 exports.__esModule = true;
 var compiler_1 = require("./modules/compiler");
 var fs_1 = require("fs");
-if (process.argv[2] === "-c" || process.argv[2] === "--compile") {
+if (process.argv[2] === "compile") {
     if (!process.argv[3] || !fs_1.existsSync(process.argv[3]))
         console.error("A file to compile must be passed.");
     else {
         var content = fs_1.readFileSync(process.argv[3]).toString("utf8");
         var compiler = new compiler_1["default"](content);
         console.log(compiler.compile());
+        // TODO: Write the compiled code to a file.
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@
 import Compiler from "./modules/compiler";
 import { readFileSync, existsSync } from "fs";
 
-if (process.argv[2] === "-c" || process.argv[2] === "--compile") {
+if (process.argv[2] === "compile") {
   if (!process.argv[3] || !existsSync(process.argv[3])) console.error("A file to compile must be passed.");
   else {
     const content: string = readFileSync(process.argv[3]).toString("utf8");
