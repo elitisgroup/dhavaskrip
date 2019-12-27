701b2267f14fdbc54b68c7c409f3299593d78af1
Chiphyr <alex@ascendcommunity.gq>
2019-12-27 17:30:00 +0000

    📄Add license/copyright notice

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+/******************************************************
+ * dhavaskrip v1.0                                    *
+ *                                                    *
+ * Copyright (c) 2019 Elitis. All rights reserved.    *
+ * Licensed under the AGPL v3.                        *
+ ******************************************************/
